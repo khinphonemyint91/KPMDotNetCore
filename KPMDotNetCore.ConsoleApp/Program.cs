414c1d31eca92375a3519c777977fce3f11339b8
Khin Phone Myint <khinphonemyint91@gmail.com>
2024-04-12 22:49:29 +0630

    Add project files.

---
+++ KPMDotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KPMDotNetCore.ConsoleApp;
+using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+//SqlConnection
+//SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+//stringBuilder.DataSource= "DESKTOP-2PIMDTH";
+//stringBuilder.InitialCatalog = "KPMDotNetCore";
+//stringBuilder.UserID = "sa";
+//stringBuilder.Password = "sa@123";
+//SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+//connection.Open();
+//Console.WriteLine("Connection open.");
+//string query = "select * from Tbl_Blog";
+//SqlCommand cmd = new SqlCommand(query, connection);
+//SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+//DataTable dt=new DataTable();
+//sqlDataAdapter.Fill(dt);
+
+//connection.Close();
+//Console.WriteLine("Connection close.");
+
+////dataset => datatable
+////datatable => datarow
+////datarow =>datacolumn
+
+//foreach (DataRow dr in dt.Rows)
+//{
+//    Console.WriteLine("Blog Id => " + dr["BlogId"]);
+//    Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+//    Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+//    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+//    Console.WriteLine("-----------------------------------------------");
+//}
+
+//ADO.Net Read
+//CRUD
+AdoDotNetExample    adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("title", "author", "content");
+//adoDotNetExample.Update(8, "test title", "test author", "test content");
+//adoDotNetExample.Delete(8);
+adoDotNetExample.Edit(8);
+adoDotNetExample.Edit(7);
+Console.ReadLine();
