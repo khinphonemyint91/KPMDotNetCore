2797a014fc5d81e3b49e403d4d336c4cf4e69c61
Khin Phone Myint <khinphonemyint91@gmail.com>
2024-05-10 20:01:05 +0630

    [HomeWork] PickAPile Json to Api

---
+++ KPMDotNetCore.WebApiWithNLayer/Features/LatHtaukBayDin/LatHtaukBayDinController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace KPMDotNetCore.WebApiWithNLayer.Features.LatHtaukBayDin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LatHtaukBayDinController : ControllerBase
+    {
+        private async Task<LatHtaukBayDin> GetDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("LatHtaukBayDin.json");
+            var model= JsonConvert.DeserializeObject<LatHtaukBayDin>(jsonStr);
+            return model;        
+
+        }
+
+
+        // api/LatHtaukBayDin/questions
+        [HttpGet("questions")]
+        public async Task<IActionResult> Questions()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.questions);
+        }
+        // api/LatHtaukBayDin/questions
+        [HttpGet("numberlists")]
+        public async Task<IActionResult> NumberLists()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.numberList);
+        }
+
+        [HttpGet("{questionNo}/{answerNo}")]
+        public async Task<IActionResult> Answer (int questionNo,int answerNo)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.answers.FirstOrDefault(x => x.questionNo == questionNo && x.answerNo == answerNo));
+        }
+
+
+
+    }
+
+    public class LatHtaukBayDin
+    {
+        public Question[] questions { get; set; }
+        public Answer[] answers { get; set; }
+        public string[] numberList { get; set; }
+    }
+
+    public class Question
+    {
+        public int questionNo { get; set; }
+        public string questionName { get; set; }
+    }
+
+    public class Answer
+    {
+        public int questionNo { get; set; }
+        public int answerNo { get; set; }
+        public string answerResult { get; set; }
+    }
+
+}
---
+++ KPMDotNetCore.WebApiWithNLayer/Features/PickAPile/PickAPileController.cs
@@ -0,0 +1,68 @@
+﻿using KPMDotNetCore.WebApiWithNLayer.Features.LatHtaukBayDin;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace KPMDotNetCore.WebApiWithNLayer.Features.PickAPile
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PickAPileController : ControllerBase
+    {
+        private async Task<PickAPile> GetPickAPileDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("PickAPile.json");
+            var model = JsonConvert.DeserializeObject<PickAPile>(jsonStr);
+            return model;
+
+        }
+
+        [HttpGet("questions")]
+        public async Task<IActionResult> GetAllQuestions()
+        {
+            var model = await GetPickAPileDataAsync();
+            return Ok(model.Questions);
+        }
+        
+        [HttpGet("{questionId}")]
+        public async Task<IActionResult> QuestionById(int questionId)
+        {
+            var model = await GetPickAPileDataAsync();
+            var answers = model.Answers.Where(x => x.QuestionId == questionId).ToList();
+            return Ok(answers);
+
+        }
+
+        [HttpGet("{questionId}/{answerId}")]
+        public async Task<IActionResult> AnswerByQuestionId(int questionId, int answerId)
+        {
+            var model = await GetPickAPileDataAsync();
+            return Ok(model.Answers.FirstOrDefault(x => x.QuestionId == questionId && x.AnswerId == answerId));
+        }
+
+    }
+
+
+    public class PickAPile
+    {
+        public Question[] Questions { get; set; }
+        public Answer[] Answers { get; set; }
+    }
+
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string QuestionName { get; set; }
+        public string QuestionDesp { get; set; }
+    }
+
+    public class Answer
+    {
+        public int AnswerId { get; set; }
+        public string AnswerImageUrl { get; set; }
+        public string AnswerName { get; set; }
+        public string AnswerDesp { get; set; }
+        public int QuestionId { get; set; }
+    }
+
+}
